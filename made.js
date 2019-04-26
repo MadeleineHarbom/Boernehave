e78268dd7a5781d158ac0162c16e66eddf1ba414
MadeleineHarbom <madeleine.harbom@gmail.com>
2019-04-26 08:58:24 +0200

    Gutten morgen

--- made.js
+++ made.js
@@ -1,22 +1,29 @@
 
+
+//vaelge tidspunkt
 let lokale = "lokale2";
 let tidspunkt = "tidspunkt1";
 //lokale2-tidspunkt1
 
-alert("Hej");
-
-
-function highlight(lokal, tidspunkt) {
+function highlightLokale(lokal, tidspunkt) {
     let fieldname = "grid-" + lokal + "-" + tidspunkt;
     console.log(fieldname);
     console.log("lokale2-tidspunkt1");
     let field = document.getElementById(fieldname);
     console.log(field);
-    if (field != undefined) {
-        alert("I have a field");
-    }
+    console.log(field);
+    field.classList.add("clicked");
+}
+highlightLokale(lokale, tidspunkt);
+
+let paeda;
+
+function highlightPaeda(paedaNummer) {
+    let fieldname = "grid-paedagog" + paedaNummer;
+    console.log(fieldname);
+    let field = document.getElementById(fieldname);
     console.log(field);
     field.classList.add("clicked");
 }
 
-highlight(lokale, tidspunkt);
+highlightPaeda(1);
