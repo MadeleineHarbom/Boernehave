c676a16bd47cfbc1834f12c634dafa7b13686ce4
Rabeea_Moussa <rabeea96@hotmail.com>
2019-04-26 15:49:21 +0200

    funktionalitet for at tilføje samt slette pædagoger fungerer nu som det skal

--- public/script.js
+++ public/script.js
@@ -5,19 +5,38 @@ function placerepaedagog(element) {
 }
 
 function paedagogplaceret(element) {
+    let fjernKnap = document.createElement("button");
+    fjernKnap.innerHTML = "Fjern";
+    let divClear = document.createElement("div");
+    divClear.style.clear = "both";
+    divClear.style.height = "0 px";
+    fjernKnap.style.float = "right";
+    fjernKnap.onclick = function() {fjernPaedagog(this.parentElement, this)};
+
     let paedagog1 = element.children[0];
-    let paedagog2 = element.children[2];
-    let paedagog3 = element.children[4];
+    let paedagog2 = element.children[1];
+    let paedagog3 = element.children[2];
 
-    if(paedagog1.innerHTML == "") {
-        paedagog1.innerHTML = initialer;
+    if(paedagog1.innerHTML == "" && initialer != "") {
+        paedagog1.innerHTML = "&nbsp;&nbsp;" + initialer + "&nbsp;&nbsp;&nbsp;&nbsp;";
+        paedagog1.appendChild(fjernKnap);
+        paedagog1.appendChild(divClear);
 
-    } else if(paedagog2.innerHTML == "") {
-        paedagog2.innerHTML = initialer;
+    } else if(paedagog2.innerHTML == "" && initialer != "") {
+        paedagog2.innerHTML = "&nbsp;&nbsp;" + initialer + "&nbsp;&nbsp;&nbsp;&nbsp;";
+        paedagog2.appendChild(fjernKnap);
+        paedagog2.appendChild(divClear);
 
-    } else if(paedagog3.innerHTML == "") {
-        paedagog3.innerHTML = initialer;
+    } else if(paedagog3.innerHTML == "" && initialer != "") {
+        paedagog3.innerHTML = "&nbsp;&nbsp;" + initialer + "&nbsp;&nbsp;&nbsp;&nbsp;";
+        paedagog3.appendChild(fjernKnap);
     }
 
     initialer = "";
+}
+
+function fjernPaedagog(parent, element) {
+    parent.removeChild(element);
+    parent.innerHTML = "";
+    console.log(element);
 }
